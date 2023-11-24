b1f3cc91d7cda96f989ffd2105cbf390e1b94859
Noel Alberto <noelalberto98@gmail.com>
2023-11-24 00:10:06 -0600

    Cluster 3d

--- T6/Main.py
+++ T6/Main.py
@@ -34,11 +34,50 @@ X = np.array(list(zip(df['price'], df['area'], df['bedrooms'])))
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
+# Lista para almacenar las puntuaciones de la silueta
+silhouette_scores = []
+
 # Iterar para diferentes números de clusters
-for n_clusters in range(2, 20):  # por ejemplo, de 2 a 20 clusters
+for n_clusters in range(2, 10):  # de 2 a 10 clusters
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
     cluster_labels = kmeans.fit_predict(X_scaled)
     
     # Calcular la puntuación de la silueta
     silhouette_avg = silhouette_score(X_scaled, cluster_labels)
-    print(f"For n_clusters={n_clusters}, the average silhouette score is: {silhouette_avg:.4f}")
+    silhouette_scores.append(silhouette_avg)
+
+# Graficar el método de la silueta
+plt.figure(figsize=(8, 6))
+plt.plot(range(2, 10), silhouette_scores, marker='o', linestyle='--')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Silhouette Score')
+plt.title('Silhouette Method')
+plt.xticks(range(2, 10))
+plt.grid(True)
+plt.show()
+
+#######################################################################################
+#Generacion del Cluster
+
+# Crear un modelo KMeans con 6 clusters
+kmeans = KMeans(n_clusters=6, random_state=42)
+
+# Ajustar el modelo a los datos y obtener las etiquetas de los clusters
+cluster_labels = kmeans.fit_predict(X_scaled)
+
+# Crear una figura 3D
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+# Graficar los puntos de datos con colores según los clusters
+scatter = ax.scatter(X_scaled[:, 0], X_scaled[:, 1], X_scaled[:, 2], c=cluster_labels, cmap='viridis', s=50)
+ax.set_xlabel('Price')
+ax.set_ylabel('Area')
+ax.set_zlabel('Bedrooms')
+
+# Agregar leyenda para los clusters
+legend1 = ax.legend(*scatter.legend_elements(), title="Clusters")
+ax.add_artist(legend1)
+
+plt.title('Clustering de datos en 3D')
+plt.show()
